3d982ec594e1407a30c29e7fe8939819eb46170f
Святослав Гранатир <granatir@ukr.net>
2020-03-07 16:52:12 +0200

    Добавьте файлы проекта.

---
+++ SortLIB/SortLIB.cpp
@@ -0,0 +1,43 @@
+﻿// SortLIB.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void bubbleSort(int* a, int n)
+{
+	for (int i = n - 1; i > 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (a[i] < a[j])
+			{
+				int b = a[i];
+				a[i] = a[j];
+				a[j] = b;
+			}
+		}
+	}
+}
+
+void shellSort(int* a, int n)
+{
+	{
+		int i, j, step;
+		int tmp;
+		for (step = n / 2; step > 0; step /= 2)
+			for (i = step; i < n; i++)
+			{
+				tmp = a[i];
+				for (j = i; j >= step; j -= step)
+				{
+					if (tmp < a[j - step])
+						a[j] = a[j - step];
+					else
+						break;
+				}
+				a[j] = tmp;
+			}
+	}
+}
---
+++ SortLIB/SortLIB.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void bubbleSort(int* a, int n);
+void shellSort(int* a, int n);
